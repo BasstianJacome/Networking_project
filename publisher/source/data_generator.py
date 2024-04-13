52d65d81bb1223f730bf6a8ec2f5e4b84a3eaac6
Vinicio Jacome <vjacomeg@my.centennialcollege.ca>
2024-04-13 18:36:46 -0400

    data_generator.py tested and working

--- publisher/source/data_generator.py
+++ publisher/source/data_generator.py
@@ -8,3 +8,12 @@ class DataGenerator:
         # Generate a random temperature value between 0 and 100
         temperature = round(random.uniform(0, 100), 2)
         return temperature
+
+# Create an instance of DataGenerator
+data_generator = DataGenerator()
+
+# Generate a temperature value
+temperature_value = data_generator.generate_value()
+
+# Print the generated temperature value
+print("Generated Temperature Value:", temperature_value)
